958401a9d775e031f6dbef66fa7f4d3e076f3cda
Aleksandr Kim <112456407+KimAleksandr09041988@users.noreply.github.com>
2025-01-12 19:18:05 +0800

    Update Introspection.py
    hotfix

--- Introspection.py
+++ Introspection.py
@@ -11,7 +11,7 @@ class obj:
 def introspection_info(obj):
     dict_ = {}
     dict_['type'] = str(type(obj)).replace("<class '", '').replace("'>", '')
-    dict_['attributes'] = dir(obj)
+    dict_['attributes'] = [i for i in dir(obj) if not callable(getattr(obj, i))]
     dict_['mettods_1'] = [i for i in dir(obj) if callable(getattr(obj, i))]
     dict_['methods_2'] = getmembers(obj, predicate=ismethod)
     dict_['module'] = obj_.__module__
@@ -20,4 +20,3 @@ def introspection_info(obj):
 if __name__ == '__main__':
     obj_ = obj()
     pprint(introspection_info(obj_))
-
